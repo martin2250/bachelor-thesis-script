a7b32b16f86a432c5aad69665f62933f9cf0801f
Martin Pittermann <martinp2250@gmail.com>
2018-11-24 23:57:58 +0100

    created awesome fit function for gain

--- lib/fitgain.py
+++ lib/fitgain.py
@@ -1,17 +1,54 @@
 import numpy as np
+import scipy.optimize
 
 
-def fit(frequency, gain):
+def fit_hybrid(frequency, gain, print_fit=False):
 
-	def bandpass(f, gain, cutoffL, orderL, cutoffH, orderH):
-		return gain /
-		(np.power(np.abs(1 + 1j * f / cutoffL), orderL)
-                    * np.power(np.abs(1 + 1j * cutoffH / f), orderH))
+	def bandpass(f, gain, cutoffL, cutoffH, orderL, orderH, b, c, d, e):
+		# subtract mean of cutL/H in log space to get better output for gain
+		lf = np.log10(f * np.sqrt(cutoffL / cutoffH))
 
-	fitresult, _ = scipy.optimize.curve_fit(bandpass, Frequency,
-                                         Gain, (10, 10000, 1, 20, 1), bounds=([0, 100, 0.5, 1, 0.5], [100, 1e6, 1000, 1000, 1000]))
+		# response of first order low and high pass filters
+		lowpass = 1 / np.abs(1 + 1j * f / cutoffH)
+		highpass = 1 / np.abs(1 + 1j * cutoffL / f)
 
-	fitresult = np.polyfit(
-		np.log10(frequency), np.log10(gain), args.fit_gain_degree)
+		# the two functions to model the gain
+		# polynomial, centered around the middle of cutL/H in log space
+		poly = (1 + b * lf + c * lf**2 + d * lf**3 + e * lf**4)
+		# theoretical model with higher order low/high passes
+		model = np.power(lowpass, orderH) * np.power(highpass, orderL)
 
-	Gain_fit = 10**np.polyval(fitresult, np.log10(Frequency_fit))
+		ratio = lowpass * highpass
+
+		output = gain * (poly * ratio + model * (1 - ratio))
+
+		return output
+
+	bandfit, _ = scipy.optimize.curve_fit(bandpass, frequency,
+                                       gain, (10, 20, 10000, 1, 1, 0, 0, 0, 0), bounds=([0, 5e-1, 5e3, 0.5, 0.5, -1, -1, -1, -1], [100, 5e3, 5e5, 20, 20, 1, 1, 1, 1]))
+
+	if print_fit:
+		labels = ['gain', 'cutoffL', 'cutoffH',
+                    'orderL', 'orderH', 'b', 'c', 'd', 'e']
+
+		for (label, value) in zip(labels, bandfit):
+					print(f'{label}: {value:0.3f}')
+
+	def gain_function(frequency):
+		return bandpass(frequency, *bandfit)
+
+	return gain_function
+
+
+def fit_simple(frequency, gain, print_fit=False):
+
+	def bandpass(f, gain, cutoffL, cutoffH, orderL, orderH):
+		return gain / (np.power(np.abs(1 + 1j * f / cutoffH), orderH) * np.power(np.abs(1 + 1j * cutoffL / f), orderL))
+
+	bandfit, _ = scipy.optimize.curve_fit(bandpass, frequency,
+                                       gain, (10, 20, 10000, 1, 1), bounds=([0, 5e-1, 5e3, 0.5, 0.5], [100, 5e3, 5e5, 20, 20]))
+
+	def gain(frequency):
+		return bandpass(frequency, *bandfit)
+
+	return gain
