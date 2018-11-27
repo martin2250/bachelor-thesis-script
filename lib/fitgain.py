8ab0d1baf4ad3427228569f8759bd43a600d8fa6
Martin Pittermann <martinp2250@gmail.com>
2018-11-27 23:12:23 +0100

    final

--- lib/fitgain.py
+++ lib/fitgain.py
@@ -1,9 +1,19 @@
+from dataclasses import dataclass
+
 import numpy as np
 import scipy.optimize
 
 
-def fit_hybrid(frequency, gain, print_fit=False):
+@dataclass
+class FitResult:
+	gain: float
+	cutoffL: float
+	cutoffH: float
+	orderL: float
+	orderH: float
 
+
+def fit_hybrid(frequency, gain, print_fit=False, return_result=False):
 	def bandpass_hybrid(f, gain, cutoffL, cutoffH, orderL, orderH, b, c, d, e):
 		# subtract mean of cutL/H in log space to get better output for gain
 		lf = np.log10(f * np.sqrt(cutoffL / cutoffH))
@@ -25,40 +35,66 @@ def fit_hybrid(frequency, gain, print_fit=False):
 
 		return output
 
-	bandfit, _ = scipy.optimize.curve_fit(bandpass_hybrid, frequency,
-                                       gain, (10, 20, 10000, 1, 1, 0, 0, 0, 0), bounds=([0, 5e-1, 5e3, 0.5, 0.5, -1, -1, -1, -1], [500, 5e3, 5e5, 5, 5, 1, 1, 1, 1]))
+	bandfit, _ = scipy.optimize.curve_fit(bandpass_hybrid, frequency, gain, (10, 20, 10000, 1, 1, 0, 0, 0, 0), bounds=([
+	                                      0, 5e-1, 5e3, 0.5, 0.5, -1, -1, -1, -1], [500, 5e3, 5e5, 5, 5, 1, 1, 1, 1]))
+
+	# drop 10% of values
+	error = (gain - bandpass_hybrid(frequency, *bandfit))**2
+	error_percentile = np.percentile(error, 90)
+	error_condition = error < error_percentile
+	frequency = frequency[error_condition]
+	gain = gain[error_condition]
+
+	bandfit, _ = scipy.optimize.curve_fit(bandpass_hybrid, frequency, gain, bandfit, bounds=(
+		[0, 5e-1, 5e3, 0.5, 0.5, -1, -1, -1, -1], [500, 5e3, 5e5, 5, 5, 1, 1, 1, 1]))
 
 	if print_fit:
 		labels = ['gain', 'cutoffL', 'cutoffH',
                     'orderL', 'orderH', 'b', 'c', 'd', 'e']
-
+		print(f'error mean: {np.mean(error)}  90th percentile: {error_percentile}')
 		for (label, value) in zip(labels, bandfit):
 					print(f'{label}: {value:0.3f}')
 
 	def gain_function(frequency):
 		return bandpass_hybrid(frequency, *bandfit)
 
-	return gain_function
+	if return_result:
+		return gain_function, FitResult(*(bandfit[:5]))
+	else:
+		return gain_function
 
 
 def bandpass(f, gain, cutoffL, cutoffH, orderL=1, orderH=1):
 	return gain / (np.power(np.abs(1 + 1j * f / cutoffH), orderH) * np.power(np.abs(1 + 1j * cutoffL / f), orderL))
 
 
-def fit_simple(frequency, gain, print_fit=False):
-	bandfit, _ = scipy.optimize.curve_fit(bandpass, frequency,
-                                       gain, (10, 20, 10000, 1, 1), bounds=([0, 5e-1, 5e3, 0.5, 0.5], [500, 5e3, 5e5, 20, 20]))
+def fit_simple(frequency, gain, print_fit=False, return_result=False):
+	bandfit, _ = scipy.optimize.curve_fit(bandpass, frequency, gain, (10, 20, 10000, 1, 1), bounds=([
+	                                      0, 5e-1, 5e3, 0.5, 0.5], [500, 5e3, 5e5, 20, 20]))
+
+	error = (gain - bandpass(frequency, *bandfit))**2
+	error_percentile = np.percentile(error, 90)
+
+	error_condition = error < error_percentile
+	frequency = frequency[error_condition]
+	gain = gain[error_condition]
+
+	bandfit, _ = scipy.optimize.curve_fit(bandpass, frequency, gain, bandfit, bounds=([
+	                                      0, 5e-1, 5e3, 0.5, 0.5], [500, 5e3, 5e5, 20, 20]))
 
 	if print_fit:
 		labels = ['gain', 'cutoffL', 'cutoffH', 'orderL', 'orderH']
-
+		print(f'error mean: {np.mean(error)}  90th percentile: {error_percentile}')
 		for (label, value) in zip(labels, bandfit):
 					print(f'{label}: {value:0.3f}')
 
-	def gain(frequency):
+	def gain_function(frequency):
 		return bandpass(frequency, *bandfit)
 
-	return gain
+	if return_result:
+		return gain_function, FitResult(bandfit[:5])
+	else:
+		return gain_function
 
 
 fit_lookup = {'hybrid': fit_hybrid, 'simple': fit_simple}
