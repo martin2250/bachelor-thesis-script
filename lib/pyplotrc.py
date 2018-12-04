0c0a9e2783f0b5240405f58a4b92681f87fbc124
martin2250 <martinp2250@gmail.com>
2018-12-04 09:24:35 +0100

    add support for pgf backend

---
+++ lib/pyplotrc.py
@@ -0,0 +1,7 @@
+import matplotlib
+
+# prevent x axis label being cut off
+matplotlib.rcParams['figure.autolayout'] = True
+matplotlib.rcParams['text.usetex'] = True
+matplotlib.rcParams['font.family'] = 'serif'
+matplotlib.rcParams['pgf.rcfonts'] = False
