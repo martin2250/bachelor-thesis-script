5944a78cedf7f80bd0df8beb781397d5edcbb19a
martin2250 <martinp2250@gmail.com>
2018-12-04 23:30:14 +0100

    fix pgf output

--- lib/pyplotrc.py
+++ lib/pyplotrc.py
@@ -1,13 +1,16 @@
 import matplotlib
 
-# prevent x axis label being cut off
 matplotlib.rcParams['figure.autolayout'] = True
 matplotlib.rcParams['font.family'] = 'serif'
-matplotlib.rcParams['pgf.rcfonts'] = True
 matplotlib.rcParams['text.usetex'] = True
 matplotlib.rcParams['pgf.preamble'] = matplotlib.rcParams['text.latex.preamble'] = [
-    r'\usepackage{amsmath}',
-    r'\usepackage{amssymb}',
-    r'\usepackage{siunitx}'
+	r'\usepackage{amsmath}',
+	r'\usepackage{amssymb}',
+	r'\usepackage{siunitx}'
 ]
-matplotlib.use('pgf')
+
+
+def plot_setup_pgf():
+	# prevent x axis label being cut off
+	matplotlib.rcParams['pgf.rcfonts'] = True
+	matplotlib.use('pgf')
